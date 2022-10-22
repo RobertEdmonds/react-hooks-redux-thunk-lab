73c0b4c09ffe70e16951fb4736134703238b89b6
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-10-22 13:04:20 -0400

    Complete

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,9 @@
 // write your CatList component here
+export default function CatList( {id, pic}){
+    console.log(pic)
+    return(
+        <>
+        <img src={pic} alt={id} />
+        </>
+    )
+}
